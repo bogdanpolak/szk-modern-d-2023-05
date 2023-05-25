0476283b908c248c30d5090847f666a2e04c3f39
Bogdan Polak <bogdanpol@gmail.com>
2023-05-25 09:37:25 +0200

    Updated TDemoThreadQueue

--- src/01-ModernDelphiDemo/Demo.Generics.ThreadQueue.pas
+++ src/01-ModernDelphiDemo/Demo.Generics.ThreadQueue.pas
@@ -4,7 +4,9 @@ interface
 
 uses
   System.SysUtils,
+  System.StrUtils,
   System.Classes,
+  System.Types,
   System.Generics.Collections,
   {-}
   DemoConsole;
@@ -13,6 +15,7 @@ type
   IDemoThreadQueue = interface
     ['{361C3D17-4433-441F-A927-A0624F45527D}']
     procedure Execute;
+    procedure Terminate;
   end;
 
   TDemoThreadQueue = class(TInterfacedObject, IDemoThreadQueue)
@@ -20,9 +23,11 @@ type
     fConsole: IConsole;
     fQueue: TThreadedQueue<string>;
     fThread: TThread;
+    fIsActive: Boolean;
     procedure OnThreadTeminate(Sender: TObject);
   public
     constructor Create(aConsole: IConsole);
+    procedure Terminate;
     destructor Destroy; override;
     procedure Execute;
   end;
@@ -34,42 +39,53 @@ begin
   if (aConsole = nil) then
     raise Exception.Create('IConsole is required');
   fConsole := aConsole;
+  fQueue := TThreadedQueue<string>.Create(10, 4294967295, 10);
 end;
 
 destructor TDemoThreadQueue.Destroy;
 begin
-  if (fQueue <> nil) then
-  begin
-    fThread.Terminate;
-    FreeAndNil(fThread);
-    fQueue.Free;
-  end;
+  Terminate();
+  fQueue.Free;
   inherited;
 end;
 
 procedure TDemoThreadQueue.Execute;
+var
+  item: string;
+  waitResult: TWaitResult;
 begin
-  if fQueue = nil then
+  if fThread = nil then
   begin
-    fQueue := TThreadedQueue<string>.Create;
-
     fThread := TThread.CreateAnonymousThread(
       procedure
       begin
         while not TThread.CheckTerminated do
         begin
-          fQueue.PushItem(Format('Bogdan Polak %s', [TimeToStr(Now)]));
-          Sleep(1000);
+          fQueue.PushItem(Format('%d %s', [1000 + random(1000),
+            TimeToStr(Now)]));
+          Sleep(1500);
         end;
       end);
+    fThread.OnTerminate := OnThreadTeminate;
     fThread.Start;
-  end
-  else
-    fConsole.Writeln('queue.PopItem: %s', [fQueue.PopItem]);
+  end;
+  waitResult := fQueue.PopItem(item);
+  fConsole.Writeln(IfThen((waitResult = wrSignaled), Format('queue.PopItem: %s',
+    [item]), 'Queue is empty ...'));
 end;
 
 procedure TDemoThreadQueue.OnThreadTeminate(Sender: TObject);
 begin
+  fIsActive := false;
+end;
+
+procedure TDemoThreadQueue.Terminate;
+begin
+  if (fThread <> nil) then
+  begin
+    fThread.Terminate;
+    FreeAndNil(fThread);
+  end;
 end;
 
 end.
